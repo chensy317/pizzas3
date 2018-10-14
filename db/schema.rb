8e9a2cfbc0add08759c28653910a96fc1d54f587
chensy317 <chensy317@126.com>
2018-10-14 06:06:50 -0400

    shan2

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_09_161431) do
-
-  create_table "orderlines", force: :cascade do |t|
-    t.integer "order_id"
-    t.integer "pizza_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_orderlines_on_order_id"
-    t.index ["pizza_id"], name: "index_orderlines_on_pizza_id"
-  end
-
-  create_table "orders", force: :cascade do |t|
-    t.string "name"
-    t.string "adress"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2018_10_02_003949) do
 
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
