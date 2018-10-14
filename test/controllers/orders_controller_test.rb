e5e8d35abf2705168e60a5392142c294d4ee9c0a
chensy317 <43165258+chensy317@users.noreply.github.com>
2018-10-14 18:10:45 +0800

    Delete orders_controller_test.rb

--- test/controllers/orders_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class OrdersControllerTest < ActionDispatch::IntegrationTest
-  test "should get view" do
-    get orders_view_url
-    assert_response :success
-  end
-
-end
